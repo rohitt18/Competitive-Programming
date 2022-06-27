ca8c2b05276c75a0235781b76fbc12e1d8ca076d
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-27 18:35:04 +0530

    variables datatypes and operators

---
+++ C++ basics/1.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello ";
+    // cout << sqrt(16);
+
+    // int a = 2;
+    // int b = 3;
+    // int sum = a + b;
+    // cout<<sum;
+
+    // char , int , float , double , bool
+
+    char ch = 'a';
+    cout<<ch<<endl;
+
+    int a = 3;
+    cout<<a<<endl;
+
+    double b = 3.5;
+    cout<<b<<endl;
+
+    bool d = false;
+    cout<<d<<endl;
+
+    // modulo operator
+    cout << 20%7 << endl;
+
+    // increment operator
+    int z = 5;
+    cout << ++z << endl; // pehle increment fir use
+    cout << z++ << endl; // pehle use fir increment
+    cout << z << endl; 
+
+    // decrement operator
+    int y = 5;
+    cout << --y << endl; // pehle decrement fir use
+    cout << y-- << endl; // pehle use fir decrement 
+    cout << y << endl;
+
+    return 0;
+}
