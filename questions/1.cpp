78fa7b6c9e13258fbded55cd61150126805e0fc2
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-28 05:33:33 +0530

    first question

---
+++ questions/1.cpp
@@ -0,0 +1,32 @@
+/*
+Given rectangle of length l and b. Print area of rectangle
+
+Constraints: 
+1 <= l <= 10^9
+1 <= b <= 10^9
+
+Input foramt:
+2 space separated integer l & b
+
+Output foramt:
+Single number which is area of rectangle
+
+Sample input:
+3 4 
+
+Sample output:
+12
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int l,b;
+    cin>>l>>b;
+    long long int area = 1LL * l * b;
+    cout<<area<<endl;
+
+    return 0;
+}
